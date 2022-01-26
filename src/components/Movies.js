8ffc2c4067ed7190cf077840fafa1ccd1ff86494
s2pid2k21 <S2pid2k21@gmail.com>
2022-01-26 17:43:14 +0800

    first commit

---
+++ src/components/Movies.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Movies() {
+  return (
+    <Container>
+    <h4>Recommended for you </h4>
+    <Content>
+     <Wrap>
+         <img src="https://lumiere-a.akamaihd.net/v1/images/minihero-encanto1_33953f85.jpeg?region=0,0,600,690" />
+     </Wrap>
+     
+     </Content>
+    </Container>
+  )
+}
+
+export default Movies
+
+const Container = styled.div`
+
+`
+const Content = styled.div`
+diplay: grid;
+gri-gap: 25px;
+grid-template-columns: repeat(4, minimax(0,1fr));
+
+`
+const Wrap = styled.div`
+
+`
